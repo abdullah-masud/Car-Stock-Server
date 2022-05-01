f8811b3e3dd0a771f15099dab724f6b0e6ef81e4
Abdullah Al Masud <masudctg00@gmail.com>
2022-05-02 02:53:34 +0600

    route created for loading data from mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +9,30 @@ require('dotenv').config();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yzwqe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const inventoriesCollection = client.db('warehouseManagement').collection('inventories');
+
+        app.get('/inventories', async (req, res) => {
+            const query = {};
+            const cursor = inventoriesCollection.find(query);
+            const inventories = await cursor.toArray();
+            res.send(inventories);
+        });
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Running warehouse server');
